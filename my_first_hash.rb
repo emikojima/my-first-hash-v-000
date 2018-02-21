0bfb856f39529eef72515e8017a365f730b2ff57
emi kojima <emi8emi@gmail.com>
2018-02-21 17:27:02 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+my_hash = {"name"=>"emi", "fave_food"=>"granola bars", "fave_color"=>"sky blue"}
   
 end
 
